5ab8b583a7e948a39715d42550aaf0eb1c1cc5b3
Yusif Aghalarli <josephagalar@gmail.com>
2022-02-25 19:41:20 +0400

    4

--- main.R
+++ main.R
@@ -66,9 +66,6 @@ vertice=V(egraph)
 #Edge density
 e_density <- edge_density(egraph, loops = FALSE)
 
-#Degree of each node
-egraph.degreee <-igraph::degree(egraph)
-
 #Centrality
 egraph.cent <- igraph::centr_betw(egraph)
 
@@ -96,4 +93,22 @@ count_multiple(egraph)
 
 
 
+#Central nodes(s) in the graph
+#There are two types of centrality measure: local and global
+#Local centrality does not take into acccount all network
+
+#The simplest measure of centrality is degree centrality. 
+#It counts how many edges each node
+V(egraph)$degree <-degree (egraph) 
+centralLocal <- V(egraph)[order(V(egraph)$degree,decreasing=TRUE)[1]] 
+#Betweenness centrality captures which nodes are important in the flow of the network. 
+#It makes use of the shortest paths in the network
+V(egraph)$betweenness <- betweenness(egraph, directed = F)
+centralGlobal <- V(egraph)[order(V(egraph)$betweenness , decreasing=TRUE)[1]] 
+
+#Longest path(s)
+egraph$longest_path<-get_diameter(egraph)
+egraph$longest_distnace <- diameter(egraph)
+#Largest clique
+egraph$largest_clique <- largest_cliques(egraph)
 
