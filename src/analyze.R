43c6aa5a8b1fae1b001e80590b73dbf5574f083e
yusif.aghalarli <josephagalar@gmail.com>
2022-02-27 22:51:53 +0400

    Finish

--- src/analyze.R
+++ src/analyze.R
@@ -9,8 +9,10 @@ egraph <- igraph::simplify(
   edge.attr.comb = list(Subject="first")
 )
 #Delete vertices with degree less than mean to simplify graph
-cut.off <- mean(degree(egraph))*2
+cut.off <- mean(degree(egraph))*4
 egraph<-delete_vertices(egraph,V(egraph)[degree(egraph)<cut.off])
+extractName <- function(x) strsplit(x, "@")[[c(1, 1)]]
+egraph<- set.vertex.attribute(egraph, "name", value= sapply(V(egraph)$name,extractName))
 
 
 #Edges
@@ -21,36 +23,38 @@ vertice=V(egraph)
 e_density <- edge_density(egraph, loops = FALSE)
 #Centrality
 egraph.cent <- igraph::centr_betw(egraph)
-
+hist(egraph.cent$res,main="The betweenness of vertices")
 #Diamter of graph
 egraph.diamter <-igraph::diameter(egraph)
 
-
+deg <- degree(egraph, mode="all")
+hist(deg, breaks=100, main="Histogram of node degree")
 
 ################################################
 ################################################
 #########Other functions from igraph############
 
+
+
+## Add edges to graph
+egraph <- add_edges(egraph, c("iris.mack","clint.dean"))
+
+##Add vertice to graph
+egraph <- add_vertices(egraph,2, name=c("yusif.aghalarli","sadiq.akhund"))
 ##Delete edges from graph
 egraph<-delete_edges(egraph,seq(1, 9, by = 2))
 
 ##Delete vertice from graph
-egraph<-delete_vertices(egraph,"yusif.aghalarli@gmail.com")
-
-## Add edges to graph
-egraph <- add_edges(egraph, c("iris.mack@enron.com","clint.dean@enron.com"))
-
-##Add vertice to graph
-egraph <- add_vertices(egraph,2, name=c("yusif.aghalarli@gmail.com","sadiq.akhund@gmail.com"))
+egraph<-delete_vertices(egraph,"yusif.aghalarli")
 
 ## Checks if graph has multiple edges 
-egraph <- add_edges(egraph, c("iris.mack@enron.com","clint.dean@enron.com"))
-egraph <- add_edges(egraph, c("iris.mack@enron.com","clint.dean@enron.com"))
+egraph <- add_edges(egraph, c("iris.mack","clint.dean"))
+egraph <- add_edges(egraph, c("iris.mack","clint.dean"))
 any_multiple(egraph)
 ## Get number of multiple eges
 count_multiple(egraph)
 ## Check if two vertices are connecetd
-are.connected(egraph,"iris.mack@enron.com","clint.dean@enron.com")
+are.connected(egraph,"iris.mack","clint.dean")
 ##Calculate transitivity
 ##global - ratio of triangles (direction disregarded) to connected triples.
 ##local - ratio of triangles to connected triples each vertex is part of.
@@ -60,7 +64,7 @@ transitivity(egraph, type="local")
 mean_distance(egraph, directed=F)
 
 #Neighbors of a vertex
-neig <-neighbors(egraph, V(egraph)[name="technology.enron@enron.com"], mode="out")
+neig <-neighbors(egraph, V(egraph)[name=="technology.enron"], mode="out")
 # From the given start vertex, take the given number of steps,
 #choosing an edge from the actual vertex uniformly randomly. 
 random_walk(egraph, start = 1, steps = 6)
@@ -72,24 +76,26 @@ random_walk(egraph, start = 1, steps = 6)
 #The simplest measure of centrality is degree centrality. 
 #It counts how many edges each node
 V(egraph)$degree <-degree (egraph) 
-centralLocal <- V(egraph)[order(V(egraph)$degree,decreasing=TRUE)[1]] 
+centralLocal <- V(egraph)[order(V(egraph)$degree,decreasing=TRUE)[1:5]] 
 
 #Betweenness centrality captures which nodes are important in the flow of the network. 
 #It makes use of the shortest paths in the network
 V(egraph)$betweenness <- betweenness(egraph, directed = F)
-centralGlobal <- V(egraph)[order(V(egraph)$betweenness , decreasing=TRUE)[1]] 
+centralGlobal <- V(egraph)[order(V(egraph)$betweenness , decreasing=TRUE)[1:5]] 
 
 #Longest path(s)
 egraph$longest_path<-get_diameter(egraph)
 egraph$longest_distance <- diameter(egraph)
 
 #Power centrality
-most_powerfull_10 = sort(round(power_centrality(egraph, exp=0.5), 2),decreasing = TRUE)[1:10]
+most_powerfull_10 = sort(round(power_centrality(egraph, exp=0.3), 2),decreasing = TRUE)[1:10]
 
 #Find community
 communities = cluster_edge_betweenness(egraph)
 largest_community=sort(sizes(communities)[1])
-dendPlot(communities, mode="hclust")
+
+plot(cluster_walktrap(egraph),egraph,vertex.label.cex=0.001)
+
 
 
 #Clique
@@ -106,8 +112,8 @@ largest_cliques(egraph.sym) # cliques with max number of nodes
 
 #Ego graphs, plot largest one
 egos <-make_ego_graph(egraph)
-largest_ego <- egos[[order(ego_size(egraph),decreasing=TRUE)[1]]]
-plot(largest_ego,edge.arrow.size=.0005,  
-     edge.width = 2, vertex.color="green",vertex.size=5)
+ego <- egos[[order(ego_size(egraph),decreasing=TRUE)[3]]]
+plot(ego, 
+     edge.width = 2, vertex.color="green",vertex.size=15)
 
 
--- src/visualize.R
+++ src/visualize.R
@@ -1,30 +1,8 @@
-
-temp<-delete_vertices(egraph,V(egraph)[degree(egraph)==0])
-temp<-V(egraph)[order(V(egraph)$degree,decreasing=TRUE)[1:5]] 
-g<-neighborhood(egraph,nodes=temp)
-
-
-extractName <- function(x) strsplit(x, "@")[[c(1, 1)]]
-
-width <- 1000
-height <- 600
-padding <- 60
 # Take only 75 nodes from the graph
-df <- igraph::get.data.frame(x = temp)
+df <- igraph::get.data.frame(x = egraph)``
+df <- sample_n(df, size = 40)
 g <- graph_from_data_frame(df)
-g <- set.vertex.attribute(g, "name", value= sapply(V(g)$name,extractName))
-print(g)
 # VISUALIZATIONS...
-## Setting the coordinates and adding a title label
-V(g)$weight <- degree(g)*3
-
-plot(g, edge.arrow.size=.0005,  edge.width = 2,
-     vertex.color="gold", vertex.size=V(g)$weight,
-     vertex.frame.color="gray", vertex.label.color="black",
-     vertex.label.cex=0.5, vertex.label.dist=0, edge.curved=0.2,  vertex.shape="circle",
-     layout=norm_coords(layout.fruchterman.reingold(g),20000,50000,0,2000))
-
-## METHOD 1: plot function
 
 ## METHOD 2: tcltk library
 width <- 1000
@@ -48,5 +26,3 @@ height <- as.numeric(tkcget(canvas, "-height"))
 tkcreate(canvas, "text", width / 2, 25, text = "Graph by S. Akhund & Y. Aghalarli",
          justify="center", font=tcltk::tkfont.create(family="helvetica",
                                                      size=20,weight="bold"))
-
-
